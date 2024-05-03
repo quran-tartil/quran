37504552837d1ea832222b423b6bb2fe88b85ee9
ESSARRAJ <essarraj.fouad@gmail.com>
2024-05-03 20:06:16 +0100

    save

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // https://stackoverflow.com/questions/42244541/laravel-migration-error-syntax-error-or-access-violation-1071-specified-key-wa
+        Schema::defaultStringLength(191);
+
         $migrationsPath = database_path('migrations');
         $paths = $this->getAllSubdirectoriesOptimized($migrationsPath);
 
