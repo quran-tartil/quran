910bdac3cc14c4af2f1459727f58382afb710779
ESSARRAJ <essarraj.fouad@gmail.com>
2024-05-05 13:16:43 +0100

    save

---
+++ database/migrations/Quran/2024_05_05_113719_create_topics_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->unsignedBigInteger('topic_category_id')->nullable(); 
+            $table->foreign('topic_category_id')->references('id')->on('topicCategories');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('topics');
+    }
+};
