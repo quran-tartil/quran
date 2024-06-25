d45390f5a14f81646c4c309262deed0c51449f4d
ESSARRAJ <essarraj.fouad@gmail.com>
2024-06-25 10:03:19 +0100

    Create dababase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,37 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // https://stackoverflow.com/questions/42244541/laravel-migration-error-syntax-error-or-access-violation-1071-specified-key-wa
+        Schema::defaultStringLength(191);
+
+        $migrationsPath = database_path('migrations');
+        $paths = $this->getAllSubdirectoriesOptimized($migrationsPath);
+        $this->loadMigrationsFrom($paths);
+
+        // ??
+        Paginator::useBootstrap();
+    }
+
+    function getAllSubdirectoriesOptimized($dir)
+    {
+        $subdirectories = [];
+
+        $items = scandir($dir);
+
+        foreach ($items as $item) {
+            if ($item !== '.' && $item !== '..') {
+                $path = $dir . DIRECTORY_SEPARATOR . $item;
+                if (is_dir($path)) {
+                    $subdirectories[] = $path;
+                    $subdirectoriesToAdd = $this->getAllSubdirectoriesOptimized($path);
+                    foreach ($subdirectoriesToAdd as $subdirToAdd) {
+                        $subdirectories[] = $subdirToAdd;
+                    }
+                }
+            }
+        }
+
+        return $subdirectories;
     }
 
     /**
