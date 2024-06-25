e678c89543b86de25c9a1e0b5528a4a3b22ca580
ESSARRAJ <essarraj.fouad@gmail.com>
2024-06-25 09:06:58 +0100

    init app

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.adminlte')
+
+@section('subtitle', 'Welcome')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Welcome')
+
+
+@section('content_body')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+
+@push('css')
+    {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
+@endpush
+
+@push('js')
+<script>
+console.log("Hi, I'm using the Laravel-AdminLTE package!"); 
+</script>
+@endpush
